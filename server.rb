25eefd4e7eebddbf688c1e9a31baf3093cfcecd1
Aidan Feldman <aidan.feldman@gmail.com>
2014-09-05 14:26:12 -0400

    exclude the hash from the color hex str
    this way it doesnt need to be url encoded

--- server.rb
+++ server.rb
@@ -18,12 +18,13 @@ end
 
 get '/api/v1.png' do
   values = params[:values].split(',').map(&:to_f)
+  line_color = param_with_default(:line_color, '4A8FED')
 
   # http://bit.ly/1qnR55Y
   blob = Sparklines.plot(values,
     dot_size: fixnum_param_with_default(:dot_size, 4),
     height: fixnum_param_with_default(:height, 30),
-    line_color: param_with_default(:line_color, '#4A8FED'),
+    line_color: "##{line_color}",
     step: fixnum_param_with_default(:step, 30)
   )
 
