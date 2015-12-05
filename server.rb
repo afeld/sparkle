7942273356f92a97224815d039ccfd0442141ec4
victoriaj13@gmail.com <victoriaj13@gmail.com>
2015-12-05 15:03:15 -0500

    added the option of changing background color or having a transparent background

--- server.rb
+++ server.rb
@@ -11,14 +11,26 @@ def fixnum_param_with_default(key, default)
   param_with_default(key, default).to_i
 end
 
+def color_param_with_default(key, default)
+  params[key]
+  if params[key] === "transparent" 
+   return params[key]
+  elsif /^([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/ === params[key] 
+    return "##{params[key]}"
+  else
+    return "##{default}"
+  end
+end
+
+
 def generate_image(file_type)
   values = params[:values].split(',').map(&:to_f)
-  background_color = param_with_default(:background_color, 'FFFFFF')
+  background_color = color_param_with_default(:background_color, 'FFFFFF')
   line_color = param_with_default(:line_color, '4A8FED')
 
   # http://bit.ly/1qnR55Y
   blob = Sparklines.plot(values,
-    background_color: "##{background_color}",
+    background_color: background_color,
     dot_size: fixnum_param_with_default(:dot_size, 4),
     height: fixnum_param_with_default(:height, 30),
     line_color: "##{line_color}",
