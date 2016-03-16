1cff37d4fc5cc8a6ed832c6289d5003102628786
Aidan Feldman <aidan.feldman@gsa.gov>
2016-03-16 11:25:16 -0400

    clean up server

--- server.rb
+++ server.rb
@@ -3,31 +3,17 @@ require 'bundler'
 
 Bundler.require
 
-def param_with_default(key, default)
-  params[key] ? params[key] : default
-end
-
-def width_with_default(val_length)
+def width_with_default(num_values)
   if params[:width]
-    params[:width].to_i / (val_length - 1)
+    params[:width].to_i / (num_values - 1)
   else
     fixnum_param_with_default(:step, 30)
   end
 end
 
 def fixnum_param_with_default(key, default)
-  param_with_default(key, default).to_i
+  (params[key] || default).to_i
 end
-
-# def color_param_with_default(key, default)
-#   if params[key] === "transparent" 
-#    params[key]
-#   elsif /^([a-f0-9]{6}|[a-f0-9]{3})$/i === params[key] 
-#     "##{params[key]}"
-#   else
-#     "##{default}"
-#   end
-# end
 
 def hex_color?(val)
   /^([a-f0-9]{6}|[a-f0-9]{3})$/i =~ val
@@ -41,15 +27,11 @@ def convert_color_param(val)
   end
 end
 
-# background_color = param_with_default(:background_color, 'FFFFFF')
-# background_color = convert_color_param(background_color)
-
-
 def generate_image(file_type)
   values = params[:values].split(',').map(&:to_f)
-  background_color = param_with_default(:background_color, 'FFFFFF')
+  background_color = params[:background_color] || 'FFFFFF'
   background_color = convert_color_param(background_color)
-  line_color = param_with_default(:line_color, '4A8FED')
+  line_color = params[:line_color] || '4A8FED'
   step_width = width_with_default(values.length)
   # http://bit.ly/1qnR55Y
   blob = Sparklines.plot(values,
